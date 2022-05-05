c9a30f452564c2399ae9b1c129431e735a95d937
whoiszxl <whoiszxl@gmail.com>
2022-05-05 18:53:22 +0800

    init.

---
+++ resources/sql/member.sql
@@ -0,0 +1,174 @@
+DROP TABLE IF EXISTS `ums_member`;
+CREATE TABLE `ums_member`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `username`                  varchar(16) NOT NULL COMMENT '会员名',
+    `password`                  varchar(100) NOT NULL COMMENT '密码',
+    `email`                     varchar(64) DEFAULT '' COMMENT '邮箱',
+    `phone`                     varchar(16) DEFAULT '' COMMENT '手机',
+    `avatar`                    varchar(200) NOT NULL COMMENT '头像',
+    `nickname`                  varchar(100) DEFAULT '' COMMENT '昵称',
+    `google_key`                varchar(32) DEFAULT '' COMMENT '谷歌验证码',
+    `google_status`             int(1) DEFAULT '0' COMMENT '谷歌验证码是否开启,默认0不开启, 1开启',
+    `real_name`                 varchar(16) DEFAULT '' COMMENT '真实姓名',
+    `online`                    tinyint(2) DEFAULT 1 COMMENT '是否在线(0:离线 1:在线)',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(64) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(64) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uni_idx_username` (`username`),
+    UNIQUE KEY `uni_idx_phone` (`phone`),
+    INDEX `idx_nickname_phone` (`nickname`, `phone`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员表';
+
+DROP TABLE IF EXISTS `ums_member_info`;
+CREATE TABLE `ums_member_info`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '用户ID',
+    `gender`                    tinyint(1) DEFAULT 0 COMMENT '性别(0:未知 1:男;2:女)',
+    `birthday`                  datetime DEFAULT NULL COMMENT '生日',
+    `slogan`                    varchar(64) DEFAULT '' COMMENT '个性标语',
+    `country_code`              varchar(5) DEFAULT '' COMMENT '国家码',
+    `country`                   varchar(100) DEFAULT '' COMMENT '国家',
+    `province`                  varchar(100) DEFAULT '' COMMENT '省份',
+    `city`                      varchar(30) DEFAULT '' COMMENT '城市',
+    `district`                  varchar(200) DEFAULT '' COMMENT '区域',
+    `school`                    varchar(200) DEFAULT '' COMMENT '学校',
+    `grade_level`               varchar(255) DEFAULT '' COMMENT '会员等级',
+    `login_count`               bigint(11) NOT NULL DEFAULT 0 COMMENT '会员登录次数',
+    `login_error_count`         bigint(11) NOT NULL DEFAULT 0 COMMENT '会员登录错误次数',
+    `last_login`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '最后登录',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(64) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(64) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`member_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员详情表';
+
+
+DROP TABLE IF EXISTS `ums_member_coin`;
+CREATE TABLE `ums_member_coin`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '用户ID',
+    `coin_quantity`             int(8) DEFAULT 0 COMMENT '币数量',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(64) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(64) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`member_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '币余额表';
+
+
+DROP TABLE IF EXISTS `ums_member_like`;
+CREATE TABLE `ums_member_like`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `like_type`                 tinyint(2) NOT NULL DEFAULT 1 COMMENT '点赞类型: 1:短视频 2:评论',
+    `like_id`                   bigint(11) NOT NULL DEFAULT 1 COMMENT '点赞类型相关记录的主键ID',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`video_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '点赞表';
+
+DROP TABLE IF EXISTS `ums_member_collection`;
+CREATE TABLE `ums_member_collection`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `video_id`                  bigint(11) NOT NULL COMMENT '视频ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `like_type`                 tinyint(2) NOT NULL DEFAULT 1 COMMENT '点赞类型: 1:短视频 2:评论',
+    `like_id`                   bigint(11) NOT NULL DEFAULT 1 COMMENT '点赞类型相关记录的主键ID',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`video_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '收藏表';
+
+
+DROP TABLE IF EXISTS `ums_member_blacklist`;
+CREATE TABLE `ums_member_blacklist` (
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `black_member_id`           bigint(11) NOT NULL COMMENT '黑名单会员ID',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(64) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(64) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_member_id` (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='会员黑名单表';
+
+DROP TABLE IF EXISTS `ums_member_follower`;
+CREATE TABLE `ums_member_follower`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '用户ID',
+    `follower_id`               bigint(11) NOT NULL COMMENT '粉丝ID',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uni_idx_member_id` (`member_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员粉丝表';
+
+
+DROP TABLE IF EXISTS `ums_member_attention_group`;
+CREATE TABLE `ums_member_attention_group`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '用户ID',
+    `name`                      varchar(64) NOT NULL COMMENT '分组名称',
+    `type`                      tinyint(1) NOT NULL COMMENT '分组类型: 1-默认分组 2-特别关注 3-私密关注',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(64) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(64) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uni_idx_member_id` (`member_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员关注分组表';
+
+DROP TABLE IF EXISTS `ums_member_attention`;
+CREATE TABLE `ums_member_attention`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '用户ID',
+    `attention_id`              bigint(11) NOT NULL COMMENT '被关注者ID',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uni_idx_member_id` (`member_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员关注表';
+
+DROP TABLE IF EXISTS `ums_member_report`;
+CREATE TABLE `ums_member_report`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '举报人的用户ID',
+    `report_member_id`          bigint(11) NOT NULL COMMENT '被举报的用户ID',
+    `report_video_id`           bigint(11) NOT NULL COMMENT '被举报的视频ID',
+    `report_comment_id`         bigint(11) NOT NULL COMMENT '被举报的评论ID',
+    `title`                     varchar(100) NOT NULL COMMENT '举报标题',
+    `content`                   varchar(200) NOT NULL COMMENT '举报内容',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除， 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '会员举报表';
---
+++ resources/sql/video.sql
@@ -0,0 +1,138 @@
+DROP TABLE IF EXISTS `vms_video`;
+CREATE TABLE `vms_video`(
+    `id`                        bigint(11) NOT NULL COMMENT '视频主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `title`                     varchar(64) NOT NULL COMMENT '标题',
+    `sub_title`                 varchar(128) NOT NULL COMMENT '副标题',
+    `tag_name_list`             varchar(256) NOT NULL COMMENT '标签名列表',
+    `cover`                     varchar(300) NOT NULL COMMENT '视频封面',
+    `video_url`                 varchar(300) NOT NULL COMMENT '视频文件地址',
+    `seconds`                   float(6,2) DEFAULT NULL COMMENT '视频秒数',
+    `width`                     int(6) DEFAULT NULL COMMENT '视频宽度',
+    `height`                    int(6) DEFAULT NULL COMMENT '视频高度',
+    `watch_type`                tinyint(1) DEFAULT 1 COMMENT '观看类型:(1:所有人 2:私密 3:好友可见 4:部分可见 5:对谁不可见)',
+    `can_watch_member`          varchar(1024) DEFAULT '' COMMENT '可见用户的id列表,使用逗号 , 分隔',
+    `cannot_watch_member`       varchar(1024) DEFAULT '' COMMENT '不可见用户的id列表,使用逗号 , 分隔',
+    `channel`                   varchar(10) NOT NULL DEFAULT '' COMMENT '来自什么渠道',
+    `address`                   varchar(100) NOT NULL DEFAULT '' COMMENT '定位地址',
+    `longitude`                 decimal(10,7) NOT NULL DEFAULT 0 COMMENT '经度',
+    `latitude`                  decimal(10,7) NOT NULL DEFAULT 0 COMMENT '纬度',
+    `ip`                        varchar(30) NOT NULL DEFAULT '' COMMENT 'ip地址',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '视频表';
+
+DROP TABLE IF EXISTS `vms_video_comment`;
+CREATE TABLE `vms_video_comment`(
+    `id`                        bigint(11) NOT NULL COMMENT '评论主键ID',
+    `video_id`                  bigint(11) NOT NULL COMMENT '视频ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `comment_text`              varchar(140) NOT NULL COMMENT '评论内容',
+    `comment_imgs`              varchar(150) NOT NULL COMMENT '评论图片',
+    `at`                        varchar(150) NOT NULL DEFAULT '' COMMENT '@了谁,下划线分割',
+    `parent_id`                 bigint(11) NOT NULL DEFAULT 0 COMMENT '评论父ID, 为0则是主评论',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '视频评论表';
+
+
+DROP TABLE IF EXISTS `vms_tag`;
+CREATE TABLE `vms_tag`(
+    `id`                        bigint(11) NOT NULL COMMENT '背景音乐主键ID',
+    `tag_name`                  varchar(100) NOT NULL COMMENT '音乐名',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '标签表';
+
+DROP TABLE IF EXISTS `vms_bgmusic`;
+CREATE TABLE `vms_bgmusic`(
+    `id`                        bigint(11) NOT NULL COMMENT '背景音乐主键ID',
+    `music_name`                varchar(100) NOT NULL COMMENT '音乐名',
+    `singer`                    varchar(100) NOT NULL COMMENT '歌手',
+    `cover`                     varchar(255) NOT NULL COMMENT '音乐封面',
+    `music_composer`            varchar(100) NOT NULL COMMENT '作曲者',
+    `music_lyricist`            varchar(100) NOT NULL COMMENT '作词者',
+    `music_copyright`           varchar(255) NOT NULL COMMENT '音乐版权',
+    `music_seconds`             float(6,2) DEFAULT NULL COMMENT '视频秒数',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '背景音乐表';
+
+DROP TABLE IF EXISTS `vms_video_counter`;
+CREATE TABLE `vms_video_counter`(
+    `video_id`                  bigint(11) NOT NULL COMMENT '视频ID,也是主键ID',
+    `counter_type`              tinyint(2) NOT NULL DEFAULT 1 COMMENT '计数类型: 1:观看量 2:转发量 3:评论量 4:点赞量',
+    `counter_value`             bigint(11) NOT NULL DEFAULT 1 COMMENT '计数值',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`video_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '视频计数表';
+
+
+DROP TABLE IF EXISTS `vms_video_watch_history`;
+CREATE TABLE `vms_video_watch_history`(
+  `id` bigint(11) NOT NULL COMMENT '主键ID',
+  `member_id` bigint(11) NOT NULL COMMENT '会员ID',
+  `video_id` bigint(11) NOT NULL COMMENT '视频ID',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '浏览历史表';
+
+
+DROP TABLE IF EXISTS `vms_video_barrage`;
+CREATE TABLE `vms_video_barrage`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `video_id`                  bigint(11) NOT NULL COMMENT '视频ID',
+    `content`                   varchar(128) NOT NULL COMMENT '弹幕内容',
+    `send_at`                   datetime DEFAULT CURRENT_TIMESTAMP COMMENT '弹幕发送时间',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '视频弹幕表';
+
+
+DROP TABLE IF EXISTS `vms_video_dynamic`;
+CREATE TABLE `vms_video_dynamic`(
+    `id`                        bigint(11) NOT NULL COMMENT '主键ID',
+    `member_id`                 bigint(11) NOT NULL COMMENT '会员ID',
+    `type`                      tinyint(1) NOT NULL DEFAULT 1 COMMENT '动态类型: 1:视频 2:专栏',
+    `resource_id`               bigint(11) NOT NULL COMMENT '动态资源ID',
+    `version`                   bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `status`                    tinyint(3) DEFAULT 0 COMMENT '业务状态',
+    `is_deleted`                tinyint(3) DEFAULT 0 COMMENT '逻辑删除 1: 已删除, 0: 未删除',
+    `created_by`                varchar(50) NOT NULL COMMENT '创建者',
+    `updated_by`                varchar(50) NOT NULL COMMENT '更新者',
+    `created_at`                datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`                datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`video_id`)
+) ENGINE = InnoDB CHARSET = utf8mb4 COMMENT '视频计数表';
